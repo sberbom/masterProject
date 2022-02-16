be3527888973829a45b894b5f2a719cc0d95ed22
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-02-16 15:40:19 +0100

    Send packts three times

--- app/src/main/java/com/example/masterproject/network/MulticastClient.kt
+++ app/src/main/java/com/example/masterproject/network/MulticastClient.kt
@@ -27,8 +27,10 @@ class MulticastClient (private val server: MulticastServer?) {
             var serverSocket = DatagramSocket()
             val msgPacket = DatagramPacket(msg.toByteArray(), msg.toByteArray().size, address, multicastPort)
             serverSocket.send(msgPacket)
+            Thread.sleep(400)
+            serverSocket.send(msgPacket)
             Log.d(TAG, "Sent message $msg")
-            Thread.sleep(800)
+            Thread.sleep(400)
             serverSocket.send(msgPacket)
             serverSocket.close()
         }catch (e: Exception) {

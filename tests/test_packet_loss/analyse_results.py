cea90ac8207bea033f59b0c75b68875d110884e0
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-02-17 11:06:23 +0100

    Show total packet loss

--- tests/test_packet_loss/analyse_results.py
+++ tests/test_packet_loss/analyse_results.py
@@ -3,21 +3,21 @@ folder = "stored_result_3_send_1"
 log_device_1 = open("./{}/log_device1.txt".format(folder), "r")
 log_device_2 = open("./{}/log_device2.txt".format(folder), "r")
 
-messages_sent = 0
+ledgers_sent = 0
 
-messages_received = 0
+ledgers_received = 0
 
 received_nonces = []
 
-number_of_rounds = 0
+requests_sent = 0
 
 for line in log_device_1:
   line_list = line.split(" ")
   filtered_line_list = list(filter(lambda el: el != "", line_list))
   if "Number of rounds:" in line:
-    number_of_rounds = int(filtered_line_list[-1])
+    requests_sent = int(filtered_line_list[-1])
   if len(filtered_line_list) > 8 and filtered_line_list[5] == "MulticastClient:" and "FULL_LEDGER" in filtered_line_list[8]:
-    messages_sent += 1
+    ledgers_sent += 1
 
 for line in log_device_2:
   line_list = line.split(" ")
@@ -25,17 +25,25 @@ for line in log_device_2:
   if len(filtered_line_list) > 6 and filtered_line_list[5] == "MulticastServer:" and "FULL_LEDGER" in filtered_line_list[6]:
     nonce = int(filtered_line_list[7])
     if not nonce in received_nonces:
-      messages_received += 1
+      ledgers_received += 1
       received_nonces.append(nonce)
 
-print("Received {0} out of {1} requests for ledger.".format(messages_sent, number_of_rounds))
+print("Received {0} out of {1} requests for ledger.".format(ledgers_sent, requests_sent))
 
-print("Packet loss of request ledger: {:.3f}".format(1 - messages_sent/number_of_rounds))
+print("Packet loss of request ledger: {:.3f}".format(1 - ledgers_sent/requests_sent))
 
-print("Received {0} out of {1} full ledgers.".format(messages_received, messages_sent))
+print("Received {0} out of {1} full ledgers.".format(ledgers_received, ledgers_sent))
 
-if messages_sent > 0:
-  packet_loss_ledger = 1 - (messages_received / messages_sent)
+if ledgers_sent > 0:
+  packet_loss_ledger = 1 - (ledgers_received / ledgers_sent)
   print("Packet loss of full ledger: {0:.3f}".format(packet_loss_ledger))
 else: 
   print("No ledgers sent.")
+
+total_received = ledgers_sent + ledgers_received
+
+total_sent = ledgers_sent + requests_sent
+
+print("Received {0} out of {1} total messages.".format(total_received, total_sent))
+
+print("Total packet loss: {}".format(1 - total_received / total_sent))

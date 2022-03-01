7899fc403765ec80d0bd277611eaada23b914119
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-03-01 14:08:47 +0100

    Script for running test

--- tests/accept_ledger_time/script.sh
+++ tests/accept_ledger_time/script.sh
@@ -1,18 +1,29 @@
-. ../common/initialize.sh
+# Gets the first two connected devices and their id
+devices=$(echo $(adb devices))
+device1=$(echo $devices | cut -f5 -d$' ')
+device2=$(echo $devices | cut -f7 -d$' ')
+device3=$(echo $devices | cut -f9 -d$' ')
+device4=$(echo $devices | cut -f11 -d$' ')
+device5=$(echo $devices | cut -f13 -d$' ')
+device6=$(echo $devices | cut -f15 -d$' ')
 rounds=5
-echo "Number of rounds: $rounds" > log_device1.txt
-echo "Number of rounds: $rounds" > log_device2.txt
+. ../commmon/initialize.sh $device1 $device2 $device3 $device4 $device5 $device6
 for i in {1..$rounds}
 do
    echo "round $i of $rounds"
    echo "round $i of $rounds" >> log_device1.txt
-   echo "round $i of $rounds" >> log_device2.txt
-   adb -s $device1 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
-   sleep 5
-   adb -s $device2 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
-   sleep 15
-   adb -s $device1 shell am force-stop com.example.masterproject
-   adb -s $device2 shell am force-stop com.example.masterproject
+   adb -s "$device1" shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
+   sleep 2
+   device_counter=2
+   for i in $device2 $device3 $device4 $device5 $device6
+   do
+      adb -s "$i" shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
+      sleep 15
+   done
+   for i in $device1 $device2 $device3 $device4 $device5 $device6
+   do
+      adb -s "$i" shell am force-stop com.example.masterproject
+   done
 done
 adb -s $device1 shell killall -2 logcat
 adb -s $device2 shell killall -2 logcat
--- tests/common/initialize.sh
+++ tests/common/initialize.sh
@@ -1,13 +1,12 @@
 # Gets the first two connected devices and their id
-devices=$(echo $(adb devices))
-device1=$(echo $devices | cut -f5 -d$' ')
-device2=$(echo $devices | cut -f7 -d$' ')
-echo $device1
-echo $device2
-# Remove all running logcats, to ensure no double logging
-adb -s $device1 shell killall -2 logcat
-adb -s $device2 shell killall -2 logcat
-adb -s $device1 logcat --clear
-adb -s $device2 logcat --clear
-adb -s $device1 logcat MulticastClient:D MulticastServer:D RegistrationHandler:D \*:S >> log_device1.txt &
-adb -s $device2 logcat MulticastClient:D MulticastServer:D RegistrationHandler:D \*:S >> log_device2.txt &
+counter=1
+ for i in "$@"
+ do
+   echo "$i"
+   # Remove all running logcats, to ensure no double logging
+   adb -s "$i" shell killall -2 logcat
+   adb -s "$i" logcat --clear
+   adb -s "$i" logcat MulticastClient:D MulticastServer:D RegistrationHandler:D \*:S >> log_device1.txt &
+   echo "Number of rounds: $rounds" > log_device$counter.txt
+   counter=$(($counter+1))
+done

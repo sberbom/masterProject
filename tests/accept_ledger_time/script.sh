a5067c3e4bae2852d1d1c0c108e8f032a02d6d71
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-03-01 11:25:04 +0100

    Create new test

---
+++ tests/accept_ledger_time/script.sh
@@ -0,0 +1,18 @@
+. ../common/initialize.sh
+rounds=5
+echo "Number of rounds: $rounds" > log_device1.txt
+echo "Number of rounds: $rounds" > log_device2.txt
+for i in {1..$rounds}
+do
+   echo "round $i of $rounds"
+   echo "round $i of $rounds" >> log_device1.txt
+   echo "round $i of $rounds" >> log_device2.txt
+   adb -s $device1 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
+   sleep 5
+   adb -s $device2 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
+   sleep 15
+   adb -s $device1 shell am force-stop com.example.masterproject
+   adb -s $device2 shell am force-stop com.example.masterproject
+done
+adb -s $device1 shell killall -2 logcat
+adb -s $device2 shell killall -2 logcat

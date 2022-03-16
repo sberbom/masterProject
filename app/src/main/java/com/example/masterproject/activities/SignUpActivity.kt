745b37a43b39740cd2ea4b2ffb3da52254e6285b
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-03-16 10:55:53 +0100

    Check if network is online first

--- app/src/main/java/com/example/masterproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/masterproject/activities/SignUpActivity.kt
@@ -87,12 +87,17 @@ class SignUpActivity: AppCompatActivity() {
             return
         }
         signupLoading(true)
-        if(password == "") {
-            runOnUiThread{passwordInvalidTextView.visibility = View.VISIBLE}
-            signupLoading(false)
-            return
+        val isOnline = networkIsOnline()
+        if (isOnline) {
+            if(password == "") {
+                runOnUiThread{passwordInvalidTextView.visibility = View.VISIBLE}
+                signupLoading(false)
+                return
+            }
+            onlineRegistration(email, password)
+        } else {
+            offlineRegistration(email)
         }
-        register(email, password)
     }
 
     private fun offlineRegistration(email: String) {
@@ -111,8 +116,7 @@ class SignUpActivity: AppCompatActivity() {
         }
     }
 
-    private fun register(email: String, password: String) {
-        // try registering online first
+    private fun onlineRegistration(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -128,18 +132,13 @@ class SignUpActivity: AppCompatActivity() {
                     returnToMainActivity(null)
                 } else {
                     Log.d(TAG, task.exception.toString())
-                    // if error contains failed to connect to, the network is offline and we do offline registration
-                    if (task.exception.toString().lowercase().contains("failed to connect to")) {
-                        offlineRegistration(email)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                       signupLoading(false)
-                        Log.d(TAG, "Firebase registration failed")
-                        Toast.makeText(
-                            baseContext, "Registration failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    // If sign in fails, display a message to the user.
+                   signupLoading(false)
+                    Log.d(TAG, "Firebase registration failed")
+                    Toast.makeText(
+                        baseContext, "Registration failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
@@ -156,6 +155,21 @@ class SignUpActivity: AppCompatActivity() {
                 signUpProgressBar.visibility = View.INVISIBLE
                 signUpButton.isEnabled = true
             }
+        }
+    }
+
+    private fun networkIsOnline(): Boolean {
+        return try {
+            val runtime = Runtime.getRuntime()
+            val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+            val hasFinished = ipProcess.waitFor(500, TimeUnit.MILLISECONDS)
+            val exitValue = try{ ipProcess.exitValue() } catch (e: IllegalThreadStateException) {-1}
+            Log.d(TAG, ipProcess.inputStream.bufferedReader().readText())
+            ipProcess.destroy()
+            return hasFinished && exitValue == 0
+        } catch (e: Exception) {
+            Log.d(TAG, e.toString())
+            false
         }
     }
 

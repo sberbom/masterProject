5dfbe67e40d9b94c7fedb28c3c504388058abfba
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-04-25 16:42:08 +0200

    500 rounds, 125 les

--- tests/inter_packet_time/script.sh
+++ tests/inter_packet_time/script.sh
@@ -2,7 +2,7 @@ devices=$(echo $(adb devices))
 device1=$(echo $devices | cut -f5 -d$' ')
 device2=$(echo $devices | cut -f7 -d$' ')
 test_path="tests/inter_packet_time"
-rounds=15
+rounds=300
 for var in "$@"
 do
    git checkout test/inter_packet_time/"$var"

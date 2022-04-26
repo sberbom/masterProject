48f6a66a5fc15f0253443359b8ae13808101671e
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-04-26 13:30:07 +0200

    Set ledger length to 1

--- app/src/main/java/com/example/masterproject/network/MulticastClient.kt
+++ app/src/main/java/com/example/masterproject/network/MulticastClient.kt
@@ -83,7 +83,7 @@ class MulticastClient  {
             val username = PKIUtils.getUsernameFromCertificate(certificate)
             val currentLedger = Ledger.availableDevices.toList()
             /** Only for testing **/
-            ConstructLedgerForTest.createLedger(100)
+            ConstructLedgerForTest.createLedger(1)
             val testLedger = ConstructLedgerForTest.ledger
             Log.d(TAG, "SENT_FULL_LEDGER")
             /***********************/

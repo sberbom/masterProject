5f9d4d4a0fd7b1b775052d8211c982b17e192e52
Endre Braut Medhus <endrebm@stud.ntnu.no>
2022-04-27 10:01:20 +0200

    Update tests

--- tests/resends/script.sh
+++ tests/resends/script.sh
@@ -2,7 +2,7 @@ devices=$(echo $(adb devices))
 device1=$(echo $devices | cut -f5 -d$' ')
 device2=$(echo $devices | cut -f7 -d$' ')
 test_path="tests/resends"
-rounds=300
+rounds=500
 for var in "$@"
 do
    git checkout test/resends/"$var"
@@ -29,9 +29,9 @@ do
          echo "round $i of $rounds" >> ./"$test_path"/"$var"/log_device1.txt
          echo "round $i of $rounds" >> ./"$test_path"/"$var"/log_device2.txt
          adb -s $device1 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
-         sleep 5
+         sleep 2
          adb -s $device2 shell am start -n com.example.masterproject/com.example.masterproject.activities.MainActivity
-         sleep 8
+         sleep 4
          adb -s $device1 shell am force-stop com.example.masterproject
          adb -s $device2 shell am force-stop com.example.masterproject
          sleep 1
